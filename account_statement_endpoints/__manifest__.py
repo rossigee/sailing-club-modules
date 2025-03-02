39a22c875902c58c20a190952fbf9b3c0b6247dc
Ross Golder <ross@golder.org>
2025-03-03 05:41:06 +0700

    Update for 15.0.

--- account_statement_endpoints/__manifest__.py
+++ account_statement_endpoints/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Bank Statements Endpoint',
-    'version': '14.0.1.0.4',
+    'version': '15.0.1.0.0',
     'author': 'Ross Golder',
     'website': 'https://golder.org/',
     'license': 'AGPL-3',
