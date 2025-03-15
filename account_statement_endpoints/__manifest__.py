9c0bfb97c3bb6aba93a7ff37fd17af3c5d3ce908
Ross Golder <ross@golder.org>
2025-03-15 08:36:27 +0700

    Update to 16.0.

--- account_statement_endpoints/__manifest__.py
+++ account_statement_endpoints/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     'name': 'Bank Statements Endpoint',
-    'version': '15.0.1.0.0',
+    'version': '16.0.1.0.0',
     'author': 'Ross Golder',
     'website': 'https://golder.org/',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'summary': 'Provides REST-like endpoints to expose details about the bank account statements.',
     'description': """
-        This module allows us to present up-to-date details from our (publicly funded) club's bank book on the club's (static) public website. As up-to-date as can be expected given we have to manually drive to 'the bank' to update the bank book, and then drive home and manually input the new lines into Odoo. F**k banks with no APIs (and poor secops)."
+        This module allows us to present up-to-date details from our (publicly funded) club's bank book on the club's (static) public website. As up-to-date as can be expected given we have to manually drive to 'the bank' to update the bank book, and then drive home and manually input the new lines into Odoo.
     """,    
     'depends': [
         'account',
